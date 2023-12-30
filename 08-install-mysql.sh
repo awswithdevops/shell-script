d06775d4fde69b2b03e25870ae7d22ef79d1720f
vinay <gkumardec05@gmail.com>
2023-12-30 18:28:00 +0530

    2nd script

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -5,8 +5,17 @@ ID=$(id -u) # root uer id
 if [ $ID -ne 0 ]
 then
     echo "ERROR:pleae run with root access "
+    exit 1 # you can give other than 0
 else
     echo "Root user"
 fi
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR: insatll mysql is failed "
+    exit 1 # you can give other than 0
+else
+    echo "insatll mysql is scucess"
+fi
