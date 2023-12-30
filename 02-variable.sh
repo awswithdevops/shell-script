8c19e03ab4eb92df9674795c61d4dae4d631e038
vinay <gkumardec05@gmail.com>
2023-12-30 13:47:23 +0530

    2nd script

--- 02-variable.sh
+++ 02-variable.sh
@@ -5,5 +5,5 @@ Person2=chandu
 
 echo "$Person1: hello $Person2,good morning"
 echo "$Person2: hello $Person1,very good morning"
-echo "$Person1: how are you $Person2"
+echo "$Person1: how are you $Person2 ?"
 echo "$Person2: i am good $Person1"
