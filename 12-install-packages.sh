35c245aba04b490a79b63997de60bcb6415e0f5a
vinay <gkumardec05@gmail.com>
2024-01-01 11:58:54 +0530

    2nd script

--- 12-install-packages.sh
+++ 12-install-packages.sh
@@ -7,6 +7,18 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if( $1 -ne 0 )
+    then
+        echo -e "$2 error:...$R Faield $N"
+    else
+        echo -e "$2 .....$G Sucess $N"
+}
+
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR:pleae run with root access $N "
@@ -15,4 +27,18 @@ else
     echo -e "$G Root user $N"
 fi
 
-echo "All arguments passed: $@"
+#echo "All arguments passed: $@"
+# git mysql postfix
+# package= git for firt time  
+
+for package in $@
+do
+    yum installing $package -y  &>> $LOGFILE
+    if [ $? -ne 0 ]
+    then
+        yum install $package-y  &>> $LOGFILE
+        VALIDATE $? "intsallation of $package"
+    else
+        echo -e "$package is already installed...$Y Skipping..$N"
+
+done
